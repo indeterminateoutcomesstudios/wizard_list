ef801e55bef9a285d6965807cbd7581aafea7c20
wiancrew <30219452+wiancrew@users.noreply.github.com>
2017-07-28 21:06:51 +0000

    Update wizard_list.py

--- wizard_list.py
+++ wizard_list.py
@@ -1,1 +1,4 @@
 >>> wizard_list = ['spider legs' , 'toe of frog' , 'eye of newt' , 'bat wing' , 'slug butter' , 'snake dandruff']
+wizard_list.append('bear burp')
+wizard_list.append('mandrake')
+wizard_list.append('swamp gas')
