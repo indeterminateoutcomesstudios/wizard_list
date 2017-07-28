082890b78fb183cdac8f5c6d3e4e4b876b60d35f
wiancrew <30219452+wiancrew@users.noreply.github.com>
2017-07-28 21:09:22 +0000

    Update wizard_list.py
    Added boilerplate notice

--- wizard_list.py
+++ wizard_list.py
@@ -1,3 +1,23 @@
+    wizard_list - The wizard is ready to work some serious magic, but he needs your help to remember what to buy!
+    Copyright (C) 2017 - Rahul Chaudhary 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Contact us at: 2520@tuta.io
+
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 >>> wizard_list = ['spider legs' , 'toe of frog' , 'eye of newt' , 'bat wing' , 'slug butter' , 'snake dandruff']
 wizard_list.append('bear burp')
 wizard_list.append('mandrake')
